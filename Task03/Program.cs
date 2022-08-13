d22c39e58087e9d7db8ff3d07f89985f1fb2f461
Mickey <mk.ru5@yandex.ru>
2022-08-13 15:47:11 +0300

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void numbers(int N)
+{
+    for (int i = 1;i<=N;i++) 
+    {
+        double result = Math.Pow(i,3);
+        Console.WriteLine(result);
+    }
+}
+numbers(5);
