ff1a11d11983e39560eef9f472c7c8f7988a6888
Mickey <mk.ru5@yandex.ru>
2022-08-13 14:13:53 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число:");
+string number_str = Console.ReadLine();
+if (number_str.Length<5 || number_str.Length>5) Console.WriteLine("Некорректное число!");
+ else
+{
+    char[] number_strReverse = number_str.ToCharArray();
+    Array.Reverse(number_strReverse);
+    string number_strNew = new string(number_strReverse);
+    if (number_str == number_strNew) Console.WriteLine("Это полиндром!");
+    else Console.WriteLine("Это НЕ полиндром!");
+}
